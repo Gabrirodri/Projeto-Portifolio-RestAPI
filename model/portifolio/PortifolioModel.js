1cbf87054346fbe6f642a9e437990d261b47edee
Gabriel Rodrigues <gabrirodri123@gmail.com>
2020-11-04 14:08:55 -0200

    metodos adicionar, deletar, editar

--- model/portifolio/PortifolioModel.js
+++ model/portifolio/PortifolioModel.js
@@ -7,4 +7,15 @@ module.exports = class PortifolioModel{
     static getId(id, callback){
         return db.query("SELECT * FROM portifolio WHERE id_portifolio = ?",[id], callback)
     }
+    static adicionar(portifolio, callback){
+        return db.query("INSERT INTO portifolio(descricao, detalhes) VALUES(?,?)",
+        [portifolio.descricao,portifolio.detalhes], callback);
+    }
+    static deletar(id,callback){
+        return db.query("DELETE FROM portifolio WHERE id_portifolio = ?", [id], callback)
+    }
+    static editar(portifolio ,callback){
+        return db.query("UPDATE portifolio SET descricao = ?, detalhes = ? WHERE id_portifolio = ?", 
+        [portifolio.descricao, portifolio.detalhes, portifolio.id_portifolio], callback)
+    }
 };
--- router/PortifolioRouter.js
+++ router/PortifolioRouter.js
@@ -32,4 +32,66 @@ router.get("/:id?",function(req,res,next){
         res.json(resposta);
     })
 })
+
+router.post("/?",function(req,res,next){
+    PortifolioModel.adicionar(req.body,function(erro,retorno){
+        let resposta = new RespostaClass();
+        if(erro){
+            resposta.erro = true;
+            resposta.msg = 'Ocorreu um erro';
+            console.log('Erro: ', erro);
+        }
+        else{
+            if(retorno.affectedRows > 0){
+                resposta.msg = "cadastro realizado com sucesso.";
+            }else{
+                resposta.erro = true;
+                resposta.msg = 'Não foi possivel realizar a operação'
+            }
+        }
+        console.log('resp: ', resposta);
+        res.json(resposta);
+    })
+})
+
+router.delete("/:id",function(req,res,next){
+    PortifolioModel.deletar(req.params.id,function(erro,retorno){
+        let resposta = new RespostaClass();
+        if(erro){
+            resposta.erro = true;
+            resposta.msg = 'Ocorreu um erro';
+            console.log('Erro: ', erro);
+        }
+        else{
+            if(retorno.affectedRows > 0){
+                resposta.msg = "Registro excluido com sucesso.";
+            }else{
+                resposta.erro = true;
+                resposta.msg = 'Não foi possivel excluir o registro'
+            }
+        }
+        console.log('resp: ', resposta);
+        res.json(resposta);
+    })
+})
+router.put("/",function(req,res,next){
+    PortifolioModel.editar(req.body,function(erro,retorno){
+        let resposta = new RespostaClass();
+        if(erro){
+            resposta.erro = true;
+            resposta.msg = 'Ocorreu um erro';
+            console.log('Erro: ', erro);
+        }
+        else{
+            if(retorno.affectedRows > 0){
+                resposta.msg = "Registro editado com sucesso.";
+            }else{
+                resposta.erro = true;
+                resposta.msg = 'Não foi possivel editar o registro'
+            }
+        }
+        console.log('resp: ', resposta);
+        res.json(resposta);
+    })
+})
 module.exports = router;
